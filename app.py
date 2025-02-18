b7440e4ba17850a54c4d9f6fab61dfcd1ab4ee30
Theodor Kier <39274446+TikoyX@users.noreply.github.com>
2025-02-18 19:24:58 +0000

    link fix

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def home():
     return "<h1>Welcome to the DevSecOps Secure App</h1>"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=False)
